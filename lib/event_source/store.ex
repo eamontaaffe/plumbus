34510357dd6416d449a812cf2764931dc09e07fc
Eamon Taaffe <eamontaaffe@gmail.com>
2017-06-08 20:27:01 +1000

    Removed index stuff

--- lib/event_source/store.ex
+++ lib/event_source/store.ex
@@ -6,15 +6,11 @@ defmodule EventSource.Store do
     defstruct stack: []
   end
 
-  # defmodule Index do
-  #   defstruct name: "", filter: fn() -> false end, stack: []
-  # end
-
   #####
   # Exteral API
 
   def start_link() do
-    GenServer.start_link(__MODULE__, [], name: __MODULE__)
+    GenServer.start_link(__MODULE__, nil, name: __MODULE__)
   end
 
   def dispatch(event) do
@@ -25,14 +21,6 @@ defmodule EventSource.Store do
     GenServer.call __MODULE__, :stack
   end
 
-  # def register_index(name, filter) do
-  #   GenServer.cast __MODULE__, {:register_index, name, filter}
-  # end
-
-  # def initialise_index(name) do
-  #   GenServer.cast __MODULE__, {:initialise_index, name}
-  # end
-
   def stack(query) do
     GenServer.call __MODULE__, {:stack, query}
   end
@@ -41,7 +29,7 @@ defmodule EventSource.Store do
   # GenServer implementation
 
   def init(_args) do
-    {:ok, %State{stack: [], indexs: []}}
+    {:ok, %State{stack: []}}
   end
 
   def handle_cast({:dispatch, event}, state) do
@@ -51,25 +39,6 @@ defmodule EventSource.Store do
   def handle_call(:stack, _from, state) do
     {:reply, state.stack, state}
   end
-
-  # def handle_cast({:register_index, name, filter}, state) do
-  #   {
-  #     :noreply,
-  #     %{state | indexs: state.indexs ++ [
-  #       %Index{
-  #         name: name,
-  #         filter: filter,
-  #         stack: [],
-  #       }
-  #     ]}
-  #   }
-  # end
-
-  # def handle_cast({:initialise_index, name}, state) do
-  #   index = Enum.find_value(state.indexs, fn(index) -> index.name == name end)
-  #   stack = Enum.filter(state.stack, fn(fact) -> index.filter.(fact) end)
-  #   {:noreply, %state}
-  # end
 
   def handle_call({:stack, query}, state) do
     result = Map.filter(state.stack, query)
